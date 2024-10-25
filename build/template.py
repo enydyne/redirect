50facd0b9a87bfbc7852c6d597e9fcda6acac380
Frank Mayer <gh@frankmayer.dev>
2024-10-25 19:28:39 +0200

    build: generate compose from template

---
+++ build/template.py
@@ -0,0 +1,37 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+
+templates_env = Environment(
+    loader=FileSystemLoader("./"),
+    lstrip_blocks=True,
+    trim_blocks=True
+)
+
+config_template = templates_env.get_template("docker-compose.yml.j2")
+
+with open("./docker-compose.yml", "w") as f:
+    refName = os.getenv("GITHUB_REF")
+    if not refName:
+        raise ValueError("GITHUB_REF not set")
+
+    repository = os.getenv("GITHUB_REPOSITORY")
+    if not repository:
+        raise ValueError("GITHUB_REPOSITORY not set")
+
+    sha = os.getenv("GITHUB_SHA")
+    if not sha:
+        raise ValueError("GITHUB_SHA not set")
+
+    domain = os.getenv("DOMAIN")
+    if not domain:
+        raise ValueError("DOMAIN not set")
+
+    f.write(config_template.render(
+        {
+            "REF_NAME": refName,
+            "REPOSITORY": repository,
+            "SHA": sha,
+            "DOMAIN": domain,
+        }))
+    f.close()
+
